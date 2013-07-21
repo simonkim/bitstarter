88a22e5c8ec8f5a75acc89e2f4242dc1e1a46c0b
Simon Kim <spamhere@kaoma.net>
2013-07-21 14:16:14 +0900

    static file handling with express

--- web.js
+++ web.js
@@ -3,26 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 
-var pathPublic = 'public/';
-
-var sendFile = function(response, filename, text) {
-  var buf;
-  console.log( 'sendFile:' + filename );
-  buf = fs.readFileSync( pathPublic + filename );
-  if ( text === true ) {
-    response.send( buf.toString() );
-  } else {
-    response.send( buf );
-  }
-};
-
-app.get('/', function(request, response) {
-    sendFile( response, 'index.html', true );
-});
-
-app.get('/images/:file', function(request, response) {
-    sendFile( response, 'images/' + request.params.file, false );
-});
+app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
